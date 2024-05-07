24ba11533542298dcbc86f8d1b1c0e92dd752b4c
Muhammed Fairooz <fairooz2150@gmail.com>
2024-05-07 19:08:55 +0530

    write code for getCartProducts

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,7 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 const bcrypt=require('bcrypt')
+var objectId=require('mongodb').ObjectID
 module.exports={
     doSignup:(userData)=>{
         return new Promise(async(resolve,reject)=>{
@@ -33,5 +34,72 @@ module.exports={
                 resolve({status:false})
             }
         })
+    },
+    addToCart:(proId,userId)=>{
+        return new Promise(async (resolve,reject)=>{
+            let userCart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+            if(userCart){
+                db.get().collection(collection.CART_COLLECTION).
+                updateOne({user:objectId(userId)},
+                {
+                    
+                        $push:{products:objectId(proId)}
+                    
+                }
+            ).then((response)=>{
+             
+                resolve()
+            })    
+            }else{
+                let cartObj={
+                    user:objectId(userId),
+                    products:[objectId(proId)]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
+                    resolve()
+                })
+            }
+        })
+
+    },
+    getCartProducts:(userId)=>{
+     
+        return new Promise(async(resolve,reject)=>{
+          let cartItems = await db.get().collection(collection.CART_COLLECTION).aggregate([
+            {
+                $match: { user: objectId(userId) }
+            }
+           
+        ])
+        .toArray();
+            resolve (cartItems[0].products);
+       
+        });
+      },
+      getProductss: (productsIds) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                console.log('productsIds:', productsIds);
+    
+                // Convert each productId to ObjectId
+                const productsObjectIds = productsIds.map(productId => objectId(productId));
+    
+                // Fetch product details using aggregation
+                let productss = await db.get().collection(collection.PRODUCT_COLLECTION).aggregate([
+                    {
+                        $match: { _id: { $in: productsObjectIds } }
+                    }
+                ]).toArray();
+    
+                console.log('productss:', productss);
+                resolve(productss);
+            } catch (error) {
+                console.error('Error in getProductss:', error);
+                reject(error);
+            }
+        });
     }
-}
+    
+      }
+
+       
--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,10 @@ const verifyLogin=(req,res,next)=>{
     res.redirect('/login')
   }
 }
+
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
  let user=req.session.user
 console.log(user)
@@ -32,6 +35,10 @@ router.get('/signup',(req,res)=>{
 router.post('/signup',(req,res)=>{
   userHelpers.doSignup(req.body).then((response)=>{
     console.log(response);
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
+
   })
 })
 router.post('/login',(req,res)=>{
@@ -50,9 +57,18 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,(req,res)=>{
-
+router.get('/cart',verifyLogin,async (req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+ let cartProductss=await userHelpers.getProductss(products)
+  console.log(cartProductss);
   res.render('user/cart')
+})
+
+router.get('/add-to-cart/:id',verifyLogin,async (req,res)=>{
+    await userHelpers.addToCart(req.params._id,req.session.user._id).then(()=>{
+   
+    res.redirect('/')
+  })
 })
 
 
