2e571671df2885ba8624a8991b58b8b67d12b6df
Muhammed Fairooz <fairooz2150@gmail.com>
2024-09-08 18:52:59 +0530

    bcrypt changed to bcryptjs

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -1,6 +1,6 @@
 var db = require("../config/connection");
 var collection = require("../config/collections");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { ObjectId } = require("mongodb");
 const moment = require("moment");
 const Razorpay = require("razorpay");
